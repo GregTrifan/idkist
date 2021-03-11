065bdf87a2319abc4adb1da4bba3587c9f6fae2c
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-03-11 14:04:11 +0200

    Refractors

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -15,7 +15,6 @@ export class ItemsService {
   async findAll(): Promise<Item[]> {
     return this.itemModel.find();
   }
-  // Upadte Service
   async update(Item: ItemDto, name: string): Promise<ItemDto> {
     try {
       const item = await this.itemModel.findOne({ name: name });
@@ -29,7 +28,6 @@ export class ItemsService {
       };
     }
   }
-  // Remove Service
   async remove(name: string): Promise<ItemDto> {
     try {
       const item = await this.itemModel.findOne({ name: name });
