7c2d97ac2085af2b308d90fd331a64a37dad5e2e
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-03-15 07:57:54 +0200

    Just gardening..

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -10,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
-    // Local Strategy for validation
+    // Local Strategy for auth validation
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
