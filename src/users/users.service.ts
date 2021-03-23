1c159721a438c70367bd748e0c3973308c3b1b03
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-03-23 13:42:43 +0200

    documented service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,12 @@ import * as bcrypt from 'bcrypt';
 export class UsersService {
   constructor(
     @InjectModel(Users.name) private usersModel: Model<UsersDocument>,
-  ) {}
+  ) { }
+  /**
+   * Create a user based on the received params
+   * @param account 
+   * @returns UserType | `{ status: 'Fail', reason: 'Username already existing' }`
+   */
   async create(account: UserType): Promise<UserType | any> {
     // Hashing password
     const hashed = await bcrypt.hash(account.password, 12);
@@ -25,6 +30,11 @@ export class UsersService {
       return { status: 'Fail', reason: 'Username already existing' };
     }
   }
+  /**
+   * Find an user based off his username
+   * @param username 
+   * @returns UserType | undefined
+   */
   async findOne(username: string): Promise<UserType | undefined> {
     return this.usersModel.findOne({ username: username });
   }
